31e6990927ece2cf5d77c334aaa765ff6328b508
AngelMdez <angelmendezcampal@gmail.com>
2018-12-20 11:44:53 +0100

    Animations init

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 $(function() {
   var text = $(".text");
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 200) {
-      text.removeClass("hidden");
-    } else {
-      text.addClass("hidden");
-    }
-  });
+  var position = $(".position");
+  text.removeClass("hidden");
+  setTimeout(
+    function() 
+    {
+      position.removeClass("hidden");
+    }, 2000);
 });
+
+
+/*$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 200) {
+    text.removeClass("hidden");
+  } else {
+    text.addClass("hidden");
+  }
+});*/
