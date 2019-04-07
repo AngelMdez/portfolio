65a29e852de509897fc0d89c6a69fafc23f73d05
AngelMdez <Brigadas123>
2019-04-07 23:15:51 +0200

    Mobile responsive and improvements on projects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 
 
+
+$(".portfolio__body__entry").on('mouseenter mouseleave', function(e){
+    $(this).children("h3").toggleClass("hidden fadeInDownAnimatedFast") ; 
+})
+
 $(function() {
   var text = $(".text");
   var position = $(".position");
@@ -14,16 +19,24 @@ $(function() {
 
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
+  var i = 0;
+  if (scroll+$(window).height() >= $('.entry__content').offset().top) {
+    $('.entry__content').children().each(function(){
+
+      var child = $(this);
+      setTimeout(function () {
+        child.addClass("slideInDownAnimated");
+      }, 500 + i);
+      i += 100;
+    })  
+  }
+
   if (scroll+$(window).height() >= $('.portfolio__title').offset().top) {
     $('.portfolio__title').addClass("slideInRightAnimated")
     setTimeout(function () {
       $('.portfolio__subtitle').addClass("slideInRightAnimated")
     }, 500);
-    
-  }  
 
-  var i = 0;
-  if (scroll+$(window).height() >= $('.portfolio__body__entry').offset().top) {
     $('.portfolio__body').children().each(function(){
 
       var child = $(this);
@@ -32,5 +45,19 @@ $(window).scroll(function() {
       }, 500 + i);
       i += 100;
     })  
+    
   }  
+
+  var i = 0;
+
+  /* if (scroll+$(window).height() >= $('.portfolio__body__entry').offset().top) {
+    $('.portfolio__body').children().each(function(){
+
+      var child = $(this);
+      setTimeout(function () {
+        child.addClass("slideInDownAnimated");
+      }, 500 + i);
+      i += 100;
+    })  
+  }   */
 });
